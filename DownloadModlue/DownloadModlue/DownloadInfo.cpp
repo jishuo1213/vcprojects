881a8ec9363343f46290cdaba35475d50810b926
Fan <jishuo1213@126.com>
2016-06-28 00:22:06 +0800

    获取大文件大小

--- DownloadModlue/DownloadModlue/DownloadInfo.cpp
+++ DownloadModlue/DownloadModlue/DownloadInfo.cpp
@@ -19,7 +19,7 @@ bool DownloadInfo::check_is_tempfile_exits()
 	if(_taccess(temp_file_path,0) == 0){
 		_tfopen_s(&fp,temp_file_path,_T("rb"));	
 		if(fp) {
-			downloaded_size = _filelength(_fileno(fp));
+			downloaded_size = _filelengthi64(_fileno(fp));
 			is_break_point_download = true;
 			fclose(fp);
 			return true;
